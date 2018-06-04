eed67aba33c85595ed753411d63169bf150764ec
Carr OConnor <carr.oconnor@gmail.com>
2018-06-04 09:50:45 -0400

    Add project files.

---
+++ Navbar/Navbar/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Navbar
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new Navbar.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
